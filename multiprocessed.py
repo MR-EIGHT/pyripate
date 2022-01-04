32ac495e6a6180580e76762ce885664ac6d8fd1c
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-01-05 02:21:19 +0330

    some bugs fixed.

--- multiprocessed.py
+++ multiprocessed.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import os
 from multiprocessing import shared_memory
 import concurrent.futures
 import threading
@@ -9,20 +10,23 @@ import requests
 from queue import Queue, Empty
 from urllib.parse import urljoin, urlparse
 
-threadLock = threading.Lock()
-lock = multiprocessing.Lock()
-
 
 class pyripate:
     counter = 0
 
     def __init__(self, arguments):
         self.url = arguments.url
+        print(self.url)
         self.limit = arguments.number
         self.root_url = '{}://{}'.format(urlparse(self.url).scheme,
                                          urlparse(self.url).netloc)
 
         self.path = str(urlparse(self.url).netloc)
+        self.path = str(self.url.replace(urlparse(self.url).scheme, '').replace('/', '').replace(':', ''))
+        if not os.path.isdir(f".\\{self.path}"):
+            os.makedirs(f".\\{self.path}")
+        self.threadLock = threading.Lock()
+        self.lock = multiprocessing.Lock()
 
         if arguments.multiprocess:
             self.pool = concurrent.futures.ProcessPoolExecutor(max_workers=arguments.parallels)
@@ -30,27 +34,22 @@ class pyripate:
             # shm = multiprocessing.shared_memory.SharedMemory(create=True,size=arguments.number * 20)
             # self.scraped_urls = shm.buf[:] = set()
             self.scraped_urls = multiprocessing.Manager().dict()
+            self.scraped_urls['counter'] = 0
+            self.scraping_queue.put(arguments.url)
 
-        elif arguments.multithread:
-            self.pool = concurrent.futures.ThreadPoolExecutor(max_workers=arguments.parallels)
-            self.scraping_queue = Queue()
-            self.scraped_urls = set()
 
     def rip(self):
-        while True:
+        while True and self.scraped_urls.get('counter') <= self.limit:
             try:
                 print(f"Current Process: {multiprocessing.current_process().name}\n")
                 current_page = self.scraping_queue.get(timeout=20)
-
                 if self.scraped_urls.get(current_page) != 1:
                     print(f"Scraping URL: {current_page}")
                     self.scraped_urls[current_page] = 1
-                    job = self.pool.submit(self.ate, current_page)
-                    job.add_done_callback(self.post_scrape)
-                    with lock:
-                        if len(self.scraped_urls) == self.limit:
-                            self.pool.shutdown(wait=True)
-                            return
+                    with self.pool as pool:
+                        pool.submit(self.ate, current_page)
+                    with self.lock:
+                        self.scraped_urls['counter'] += 1
             except Empty:
                 return
             except Exception as e:
@@ -58,29 +57,52 @@ class pyripate:
                 continue
 
     def ate(self, url):
+        print('hey')
         try:
             content = requests.get(url, timeout=(3, 30))
-            return content
+            self.post_scrape(content)
+
         except requests.RequestException:
             return
 
-    def post_scrape(self, cont):
-        content = cont.result()
-        if content and content.status_code == 200:
-            with open(f"{self.path} / {content.url}", 'w') as file:
-                file.write(cont.content)
+    def post_scrape(self, content):
+        if content.status_code == 200:
+            # content_type = content.headers['content-type']
+            # extension = mimetypes.guess_extension(content_type)
+            path = urlparse(content.url).path
+            # ext = os.path.splitext(path)[1]
+            # print(path)
+            with open(
+                    f"./{self.path.replace('/', '').replace(':', '')}/{content.url.replace('/', '').replace(':', '') + ('.html' if not '.' in content.url[-4:] or path == '/' else '')}",
+                    'wb') as file:
+                file.write(content.content)
             self.parse_links(content.text)
-            self.scrape_info(content.text)
+            # self.scrape_info(content.text)
 
     def parse_links(self, html):
-        soup = BeautifulSoup(html,'html.parser')
+        soup = BeautifulSoup(html, 'html.parser')
         anchor_tags = soup.find_all('a', href=True)
+        source_tags = soup.find_all('img') + soup.find_all('a')
         for link in anchor_tags:
             url = link['href']
             if url.startswith('/') or url.startswith(self.root_url):
                 url = urljoin(self.root_url, url)
-                if url not in self.scraped_pages:
-                    self.crawl_queue.put(url)
+
+                if url not in self.scraped_urls:
+                    if '.' not in url[-5:] and url[-5:] != '.html':
+                        self.scraping_queue.put(url)
+                    else:
+                        self.scraped_urls[url] = 1
+                        self.ate(url)
+
+        for link in source_tags:
+            url = link['src']
+            print(url)
+            url = urljoin(self.root_url, url)
+            if url not in self.scraped_urls:
+                self.scraped_urls[url] = 1
+                self.ate(url)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -98,7 +120,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     scrapper = pyripate(args)
-    pyripate.rip()
-    pyripate.status()
+    scrapper.rip()
+    # scrapper.status()
 
     print(args)
