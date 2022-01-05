812dad20c713e7b82064cf54ef1ec1c2b2a1ff50
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-01-05 21:56:12 +0330

    initial bowsablity feature implemented.

---
+++ browsabled.py
@@ -0,0 +1,18 @@
+import os
+from bs4 import BeautifulSoup
+
+
+class Browsabled:
+    def __init__(self, path):
+        self.path = path
+
+    def make_links_relative(self):
+        for file in os.listdir(self.path):
+            with open(self.path + file, 'rb') as f:
+                text = f.read()
+            soup = BeautifulSoup(text, 'html.parser')
+            anchor_tags = soup.find_all('a', href=True)
+            for link in anchor_tags:
+                url = link['href']
+
+    def __get_file_name__(self, url):
